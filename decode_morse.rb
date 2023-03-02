b6ccdc4680f04f205522014b4a752f06cb23c995
Teklay <teklay@gmail.com>
2023-03-03 02:03:26 +0300

    Fix linters

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,25 +1,25 @@
 def decode_char(character)
-    char = [
-        [".-","A"], ["-...","B"], ["-.-.","C"], ["-..","D"], [".","E"], ["..-.","F"], ["--.","G"], ["....","H"], ["..","I"], [".---","J"], ["-.-","K"], [".-..","L"], ["--","M"], ["-.","N"], ["---","O"], [".--.","P"], ["--.-","Q"], [".-.","R"], ["...","S"], ["-","T"], ["..-","T"], ["...-","V"], [".--","W"], ["-..-","X"], ["-.--","Y"], ["--..","Z"]
-    ]
-    char.each do |c|
-        if c[0] == character
-            return c[1]
-        end
-    end
+  char = [['.-', 'A'], ['-...', 'B'], ['-.-.', 'C'], ['-..', 'D'],
+          ['.', 'E'], ['..-.', 'F'], ['--.', 'G'], ['....', 'H'], ['..', 'I'],
+          ['.---', 'J'], ['-.-', 'K'], ['.-..', 'L'], ['--', 'M'], ['-.', 'N'],
+          ['---', 'O'], ['.--.', 'P'], ['--.-', 'Q'], ['.-.', 'R'], ['...', 'S'],
+          ['-', 'T'], ['..-', 'T'], ['...-', 'V'], ['.--', 'W'], ['-..-', 'X'], ['-.--', 'Y'], ['--..', 'Z']]
+  char.each do |c|
+    return c[1] if c[0] == character
+  end
 end
 
 def decode_word(word)
-    word = word.split(" ")
-    word.each do |w|
-        print decode_char(w)
-    end
+  word = word.split
+  word.each do |w|
+    print decode_char(w)
+  end
 end
 
 def decode_morse(morse_code)
-    morse_code = morse_code.split("  ")
-    morse_code.each do |m|
-        decode_word(m)
-        print " "
-    end
+  morse_code = morse_code.split('  ')
+  morse_code.each do |m|
+    decode_word(m)
+    print ' '
+  end
 end
