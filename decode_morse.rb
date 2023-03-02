2513a8705f007ff3df91b7f58d50f54bee745736
Teklay <teklay@gmail.com>
2023-03-03 01:37:41 +0300

    Create a ruby file to decode morse codes

---
+++ decode_morse.rb
@@ -0,0 +1,25 @@
+def decode_char(character)
+    char = [
+        [".-","A"], ["-...","B"], ["-.-.","C"], ["-..","D"], [".","E"], ["..-.","F"], ["--.","G"], ["....","H"], ["..","I"], [".---","J"], ["-.-","K"], [".-..","L"], ["--","M"], ["-.","N"], ["---","O"], [".--.","P"], ["--.-","Q"], [".-.","R"], ["...","S"], ["-","T"], ["..-","T"], ["...-","V"], [".--","W"], ["-..-","X"], ["-.--","Y"], ["--..","Z"]
+    ]
+    char.each do |c|
+        if c[0] == character
+            return c[1]
+        end
+    end
+end
+
+def decode_word(word)
+    word = word.split(" ")
+    word.each do |w|
+        print decode_char(w)
+    end
+end
+
+def decode_morse(morse_code)
+    morse_code = morse_code.split("  ")
+    morse_code.each do |m|
+        decode_word(m)
+        print " "
+    end
+end
